f7a1b9ef514695158cb0d197d6d4846cbee377cc
sarputharaj <samson.arputharaj@hitachivantara.com>
2019-09-25 15:14:59 +0530

    docker

--- dockerswarm-elk-monitoring-automation/ec2-dockerswarm-elk.sh
+++
@@ -1,4 +0,0 @@
-ansible-playbook playbook.yml 
-ansible-playbook -i hosts dockerswarm/playbook.yml --key-file="ubuntukey.pem"
-ansible-playbook -i elk/hosts elk.yml --key-file="ubuntukey.pem"
-ansible-playbook -i hosts filebeat.yml --key-file="ubuntukey.pem" 
